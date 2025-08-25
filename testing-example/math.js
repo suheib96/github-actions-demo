6d70bef2c3c9c6ca21c1248d1076fcbc503e7f80
Suheib Marzouka <suheibmarzouka@Mac.fritz.box>
2025-08-25 11:10:35 +0200

    minimale Taschenrechner applikation gebaut mit workflow

---
+++ testing-example/math.js
@@ -0,0 +1,19 @@
+function addieren(a,b){
+    return a + b
+}
+
+function subtrahieren(a,b){
+    return a - b
+}
+
+function multiplizieren(a,b){
+    return a * b
+}
+function dividieren(a,b){
+    return a / b
+}
+module.exports = {
+    addieren,
+    subtrahieren,
+    multiplizieren,
+    dividieren}
---
+++ testing-example/math.test.js
@@ -0,0 +1,20 @@
+const {addieren, subtrahieren, multiplizieren, dividieren} = require("./math.js")
+test("addiert 7 + 2, ergebnis soll 9 sein", () => {
+    expect(addieren(7,2)).toBe(9)
+})
+
+test("addiert 7,2 soll NICHT 10 ergeben", () => {
+    expect(addieren(7,2)).not.toBe(10)
+})
+
+test("subtrahiert 7,5, ergebniss soll 2 sein", () => {
+    expect(subtrahieren(7,5)).toBe(2)
+})
+
+test("multipliziert 7,5, ergebniss soll 35 sein", () => {
+    expect(multiplizieren(7,5)).toBe(35)
+})
+
+test("dividieren 10,2, ergebniss soll 5 sein", () => {
+    expect(dividieren(10,2)).toBe(5)
+})
